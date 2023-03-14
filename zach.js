cd76b7bc73312ad3a0fbc84beea6f0a2147e8a7e
Zach Hanson <zachhansonitp@gmail.com>
2023-03-13 21:33:28 -0500

    added Weather API documentation

---
+++ zach.js
@@ -0,0 +1,47 @@
+
+
+let myCityID = ""
+let myHotel = ""
+
+const axios = require("axios");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const options = {
+//	method: 'GET',
+//	headers: {
+	//	'X-RapidAPI-Key': '341879f333msh7944b28a1de46b5p153fd3jsn7851a3a193d5',
+		///'X-RapidAPI-Host': 'priceline-com.p.rapidapi.com'
+	//}
+//};
+
+//fetch('https://priceline-com.p.rapidapi.com/hotels/city/search?q=Houston', options)
+	//.then(response => response.json())
+	//.then(response => console.log(response))
+    //console.log()
+	
+
+
+    //.then((response) => response.json())
+    //  .then(myCityID= data.cityID)
+             
+        //  console.log (data.cityID) 
+         // .then (fetch('https://priceline-com.p.rapidapi.com/hotels/' + myCityID + '?checkin_date=2023-07-13&checkout_date=2023-07-15&rooms=1&offset=0&currency=USD', options)
+         // .then((response) => response.json())
+         // .then((data) => this.showmyHotels(data)))
+
+         // showmyHotels: function(data) {
+        //    const myHtoel1= data.hotels.list[0].name
+        //    const myHotel2= data.hotels.list[1].name
+         //   const myHotel3= data.hotels.list[2].name
+          //}
