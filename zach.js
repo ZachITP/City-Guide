6a6f1872613929b331dc36563cb46c7d125b2bd3
Derick Dspain <derickdspain@yahoo.com>
2023-03-15 17:18:30 -0500

    fixed one issue

--- zach.js
+++ zach.js
@@ -1,113 +1,55 @@
-
- //Airbnb API
- const optionsA = {
-
+//Airbnb API
 
 const optionsA = {
-
-    method: 'GET',
-	headers: {
-        'X-RapidAPI-Key': 'e22f94d494msh38033a92e3d0750p1548a6jsn35a14fbbd1d8',
-		'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e22f94d494msh38033a92e3d0750p1548a6jsn35a14fbbd1d8",
+    "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+  },
 };
+async function getshowmyHotels() {
+  let city = document.querySelector("#content").value;
+  console.log("city", city);
 
-  
+  await fetch(
+    "https://airbnb13.p.rapidapi.com/search-location?location=" +
+      city +
+      "&checkin=2023-04-15&checkout=2023-04-20&adults=1&children=0&infants=0&page=1&currency=USD",
+    optionsA,
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "e22f94d494msh38033a92e3d0750p1548a6jsn35a14fbbd1d8",
+        "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      console.log("\n");
 
-// //(console.log(city))
-// fetch('https://airbnb13.p.rapidapi.com/search-location?location=' + city + '&checkin=2023-04-15&checkout=2023-04-20&adults=1&children=0&infants=0&page=1&currency=USD', optionsA)
-// 	.then(response => response.json())
-// 	//.then(response => console.log(response))
-//     //.then (console.log(city))
+      const deeplink = response.results[0].deeplink;
+      const Airb = response.results[0].name;
+      const rating = response.results[0].rating;
 
-//           .then((response) =>{
-    //             console.log('response',response);
-    
-    
-    //             const {deeplink}= response.deeplink[0];
-    //             const {Airb}= response.results[0].name;
-    //             const {rating}= response.results[0].rating;
-    //             //Query all the weather data and pass it to the app
-    
-    //             document.querySelector("#deeplink").innerText = deeplink;
-    //             document.querySelector("#name").innerText = Airb;
-//             document.querySelector("#rating").innerText = rating;
-//           });
+      //Query all the weather data and pass it to the app
 
-//const { icon, description } = data.weather[0];
+      //Query all the weather data and pass it to the app
 
-// function showmyHotels(response) {
-    //const {response_time}= response.headers;
-    //const {deeplink}= response.deeplink[0];
-    //const {Airb}= results(0).name;
-    //const {rating}= results(0).rating;
-    //Query all the weather data and pass it to the app
-    //document.querySelector("#deeplink").innerText = deeplink;
-    //document.querySelector("#name").innerText = Airb;
-    //document.querySelector("#rating").innerText = rating;
-    
-    // }
-    
-    
-    
-    
-    
-    
-    
-
-    async function getshowmyHotels(){
-        let city = document.querySelector('#content').value;
-        console.log('city',city)
-        
-        await fetch('https://airbnb13.p.rapidapi.com/search-location?location=' + city + '&checkin=2023-04-15&checkout=2023-04-20&adults=1&children=0&infants=0&page=1&currency=USD', optionsA,{
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'e22f94d494msh38033a92e3d0750p1548a6jsn35a14fbbd1d8',
-            'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
-        }
-        
-    })
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-        console.log("\n");
-
-        const deeplink= response.results[0].deeplink;
-            const Airb= response.results[0].name;
-            const rating= response.results[0].rating;
-
-
-        //Query all the weather data and pass it to the app
-
-            //Query all the weather data and pass it to the app
-
-        document.querySelector('.deeplink').innerText = deeplink;
-        document.querySelector('.name').innerHTML = Airb;
-        document.querySelector('.rating').innerText = rating;
-        console.log('deeplink',deeplink)
-        console.log('Airb',Airb)
-        console.log('rating',rating)
-        console.log(document.querySelector('#name'))
-
-
-         
-
-         // add all countries to select element
-         //response.response.forEach(c => {
-            //const option = document.createElement('option');
-           // option.innerHTML = c.country;
-            //document.getElementById('#deeplink').appendChild(option);
-
-});
-
-
+      document.querySelector(".deeplink").innerText = deeplink;
+      document.querySelector(".name").innerHTML = Airb;
+      document.querySelector(".rating").innerText = rating;
+      console.log("deeplink", deeplink);
+      console.log("Airb", Airb);
+      console.log("rating", rating);
+      console.log(document.querySelector("#name"));
+    });
 }
 
 //Event listenor to diplay info on page
-
 document
   .querySelector(".navigation button")
-  .addEventListener("click", function (){
-
-      getshowmyHotels();
+  .addEventListener("click", function () {
+    getshowmyHotels();
   });
